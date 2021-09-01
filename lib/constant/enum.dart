6ba2583c74ab4bbcb94d41fa4317455ff4ceab4a
alena <alena_kominch@mail.ru>
2021-09-01 19:15:49 +0300

    add parameters enums

--- lib/constant/enum.dart
+++ lib/constant/enum.dart
@@ -3,4 +3,8 @@ enum Destination { Randomizer, Search, Favourites, Settings }
 enum ActivityType { recreational, cooking, education, relaxation,
   social, charity, music, busywork, diy}
 
+enum CostType { free, cheap, medium, expensive}
 
+enum GroupType { solo, small_company, big_company}
+
+enum AccessibilityType { novice, junior, middle, senior}
